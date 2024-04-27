610e6f78fe57349f9d5ee94a8b4e49f026eaa13d
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-27 20:45:32 +0530

    makes map.js readable

--- src/map.js
+++ src/map.js
@@ -1,30 +1,23 @@
-//creating a map function
 const map = function(keyArray,valueFunction){
-    //initilaizing an empty array to store values
+    
     var valueArray =[];
-    //running a for loop to obtain values for each element
-    for(let i=0;i<keyArray.length;i++){
-        let value = valueFunction(keyArray[i]);
-        //adding elements to valueArray
+    
+    for(let index=0;index<keyArray.length;index++){
+
+        let value = valueFunction(keyArray[index]);
+        
         valueArray.push(value);
     }
-    //returning the final valueArray
+    
     return valueArray;
-}
-
-//function to obtain values
-const valueFunction = function(keyArrayValue){
-    return keyArrayValue*3;
 }
 
 var array = [1,2,3,4];
 
-//obtaining values from map function
-var mappedValue = map(array,valueFunction);
+var mappedValue = map(array, num => num*3);
+
 console.log(mappedValue);
 
-//obtaining values from builtin function
-var usingMap = array.map(function(num){
-    return num*3;}
-)
+var usingMap = array.map(num=> num*3 );
+
 console.log(usingMap);
